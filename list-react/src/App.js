cbe4df3695f2cef6ae5f51e6a474d2257ab796ee
Abhinav Upadhyay <abhinavupadhyay822@gmail.com>
2023-03-02 16:45:05 +0530

    Added some files.

--- list-react/src/App.js
+++ list-react/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Title from './components/Title';
+import Loading from './components/Loading';
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= {isLoading ? 'load-page' : 'body-page'}>
+      {isLoading ? <Loading /> : <Title />}
+
     </div>
   );
 }
---
+++ list-react/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../compo.css'
+const Loading = () => {
+  return (
+    <div className='load-page'>
+        <h1 className='load'>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading

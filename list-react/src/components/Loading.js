062b8f00f86765328738196b743ea9df4461c540
Abhinav Upadhyay <abhinavupadhyay822@gmail.com>
2023-03-05 13:52:43 +0530

    Made a new Loading page

--- list-react/src/components/Loading.js
+++ list-react/src/components/Loading.js
@@ -2,9 +2,13 @@ import React from 'react'
 import '../compo.css'
 const Loading = () => {
   return (
-    <div className='load-page'>
-        <h1 className='load'>Loading...</h1>
-    </div>
+    <div className='load-main'>
+
+   
+      <div className='lds-hourglass'>
+      </div>
+
+   </div>
   )
 }
 
